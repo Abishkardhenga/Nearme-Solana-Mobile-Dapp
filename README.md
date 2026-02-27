c1d19c51bbec4f70273a36d9a366d77306badfc2
Abishkardhenga <Copywriterayan47@gmail.com>
2026-02-27 23:03:20 +0545

    refactor: remove old project structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# Nearme-Solana-Mobile-Dapp
-Nearme is a Google Maps for Solana . Discover nearby merchants and pay with SOL/USDC
--- src/hooks/useColorScheme.tsx
+++
@@ -1,7 +0,0 @@
-import { useColorScheme as useNativeColorScheme } from 'react-native';
-import { ColorScheme } from '@/types';
-
-export function useColorScheme(): ColorScheme {
-  const colorScheme = useNativeColorScheme();
-  return colorScheme === 'dark' ? 'dark' : 'light';
-}
